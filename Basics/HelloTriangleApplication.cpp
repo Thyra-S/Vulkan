ec2e7331db9518179c997f1dfe688ebe969b3f95
Thyra-S <thysesimons@gmail.com>
2026-03-20 11:59:17 -0600

    Implemented Physical device and queueing

--- Basics/HelloTriangleApplication.cpp
+++ Basics/HelloTriangleApplication.cpp
@@ -1,27 +1,302 @@
 #include "HelloTriangleApplication.h"
+#include <map>
 
-// Entry point for the application
-void HelloTriangleApplication::run() {
-    initVulkan();
-    mainLoop();
-    cleanup();
+/*---------- CLASS METHODS ----------*/
+
+/*---------- INTIALIZATION / CLEANUP METHODS ----------*/
+
+// Initialize GLFW window
+void HelloTriangleApplication::initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 }
 
 // Initialize Vulkan instance
 void HelloTriangleApplication::initVulkan() {
+    createInstance();
+    setupDebugMessenger();
+}
+
+// Pick a physical device (GPU) that supports Vulkan
+void HelloTriangleApplication::pickPhysicalDevice() {
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+    // Find how many devices there are that support Vulkan
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    if (deviceCount == 0) throw std::runtime_error("failed to find GPUs with Vulkan support!");
+    
+	// If there are devices, store them in a vector and enumerate them
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+	// If any devices are suitable, pick the first one and break out of the loop. Otherwise, throw an error.
+    // Use an ordered map to automatically sort candidates by increasing score
+    std::multimap<int, VkPhysicalDevice> candidates;
+
+    for (const auto& device : devices) {
+        if (isDeviceSuitable(device)) {
+            physicalDevice = device;
+            break;
+        }
+    }
+
+    if (physicalDevice == VK_NULL_HANDLE) {
+        throw std::runtime_error("failed to find a suitable GPU!");
+    }
+
+	// Alternative method to pick the best device based on a scoring system
+    //for (const auto& device : devices) {
+    //    int score = rateDeviceSuitability(device);
+    //    candidates.insert(std::make_pair(score, device));
+    //}
+
+    //// Check if the best candidate is suitable at all
+    //if (candidates.rbegin()->first > 0) {
+    //    physicalDevice = candidates.rbegin()->second;
+    //}
+    //else {
+    //    throw std::runtime_error("failed to find a suitable GPU!");
+    //}
+}
+
+/*Check if a physical device is suitable for our needs*/
+bool HelloTriangleApplication::isDeviceSuitable(VkPhysicalDevice device) {
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return indices.isComplete();
+}
+
+/*Rate device suitability based on its features and properties, higher score is better */
+//int HelloTriangleApplication::rateDeviceSuitability(VkPhysicalDevice device) {
+//    VkPhysicalDeviceProperties deviceProperties;
+//    vkGetPhysicalDeviceProperties(device, &deviceProperties);
+//    VkPhysicalDeviceFeatures deviceFeatures;
+//    vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+//
+//    int score = 0;
+//
+//    // Discrete GPUs have a significant performance advantage
+//    if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+//        score += 1000;
+//    }
+//
+//    // Maximum possible size of textures affects graphics quality
+//    score += deviceProperties.limits.maxImageDimension2D;
+//
+//    // Application can't function without geometry shaders
+//    if (!deviceFeatures.geometryShader) {
+//        return 0;
+//    }
+//
+//    return score;
+//}
+
+HelloTriangleApplication::QueueFamilyIndices HelloTriangleApplication::findQueueFamilies(VkPhysicalDevice device) {
+    // Logic to find graphics queue family
+    QueueFamilyIndices indices;
+
+    // Logic to find queue family indices to populate struct with
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily = i;
+        }
+
+        if (indices.isComplete()) {
+            break;
+        }
+
+        i++;
+    }
+
+    return indices;
 
 }
 
-// Main rendering loop
-void HelloTriangleApplication::mainLoop() {
-    while (true) {
-        // Poll for window events here
+// Create Vulkan instance
+void HelloTriangleApplication::createInstance() {
+    if (enableValidationLayers && !checkValidationLayerSupport()) {
+        throw std::runtime_error("validation layers requested, but not available!");
+    }
+
+	// Application info (optional, but may provide some optimization opportunities for driver)
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Hello Triangle";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	// Create Vulkan instance
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+	// Get required extensions from GLFW and validation layers, and add them to the create info
+    auto extensions = getRequiredExtensions();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
+
+	// Debug create info, only used if validation layers are enabled
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+    if (enableValidationLayers) {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        populateDebugMessengerCreateInfo(debugCreateInfo);
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+    }
+    else {
+        createInfo.enabledLayerCount = 0;
+
+        createInfo.pNext = nullptr;
+    }
+
+	// Create the Vulkan instance, throw an error if it failss
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create instance!");
     }
 }
 
 // Cleanup Vulkan resources
 void HelloTriangleApplication::cleanup() {
+    if (enableValidationLayers) {
+       DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+    }
 
+    vkDestroyInstance(instance, nullptr);
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+}
+/*---------- END OF INITIALIZATION / CLEANUP METHODS ----------*/
+
+/*---------- RENDERING METHODS ----------*/
+
+// Main rendering loop
+void HelloTriangleApplication::mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+}
+
+/*---------- END OF RENDERING METHODS ----------*/
+
+/*---------- VALIDATION LAYERS / DEBUG ----------*/
+
+// Checks what validation layers are supported by device
+bool HelloTriangleApplication::checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Returns a list of the required extensions.
+std::vector<const char*> HelloTriangleApplication::getRequiredExtensions() {
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	if (enableValidationLayers) {
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    return extensions;
+}
+
+// Setup the debug messenger for validation layers
+void HelloTriangleApplication::setupDebugMessenger() {
+    if (!enableValidationLayers) return;
+	// else, set up the debug messenger
+    VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+	populateDebugMessengerCreateInfo(createInfo);
+
+    if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) 
+    {
+        throw std::runtime_error("failed to set up debug messenger!");
+    }
+
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT 
+        | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT 
+        | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo.pfnUserCallback = debugCallback;
+    createInfo.pUserData = nullptr; // Optional
+
+    if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+        throw std::runtime_error("failed to set up debug messenger!");
+    }
+}
+
+// Helper method to load the function pointer for creating the debug messenger, since it's an extension function 
+// and not automatically loaded by Vulkan
+VkResult HelloTriangleApplication::CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    }
+    else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void HelloTriangleApplication::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+    createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo.pfnUserCallback = debugCallback;
+}
+
+
+
+/*---------- END OF VALIDATION LAYERS / DEBUG ----------*/
+
+/*---------- END OF CLASS METHODS ----------*/
+
+/*---------- MAIN METHOD / ENTRY POINT ----------*/
+
+// Entry point for the application
+void HelloTriangleApplication::run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
 }
 
 // Main function
--- Basics/HelloTriangleApplication.h
+++ Basics/HelloTriangleApplication.h
@@ -1,11 +1,17 @@
 #ifndef HelloTriangleApplication_h
 #define HelloTriangleApplication_h
 
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <cstring>
+#include <optional>
+
+using namespace std;
 
 class HelloTriangleApplication {
 public:
@@ -13,13 +19,99 @@ public:
     void run();
 
 private:
+	/*---------- GLOBAL VARIABLES ----------*/
+	VkInstance instance;
+
+	GLFWwindow* window;
+	const uint32_t WIDTH = 2400;
+	const uint32_t HEIGHT = 1800;
+
+	VkDebugUtilsMessengerEXT debugMessenger;
+
+	struct QueueFamilyIndices {
+		std::optional<uint32_t> graphicsFamily;
+		bool isComplete() {
+			return graphicsFamily.has_value();
+		}
+	};
+
+	/*---------- INITIALIZATION / CLEANUP METHODS ----------*/
+
 	// initialize Vulkan instance
     void initVulkan();
 
-	// main rendering loop
-    void mainLoop();
+	// pick a physical device (GPU) that supports Vulkan
+	void pickPhysicalDevice();
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+
+	// check if a physical device is suitable for our needs
+	bool isDeviceSuitable(VkPhysicalDevice device);
+
+	// Rate device suitability based on its features and properties, higher score is better
+	//int rateDeviceSuitability(VkPhysicalDevice device);
+
+	// create Vulkan instance
+	void createInstance();
+
+	// initialize GLFW window
+	void initWindow();
 
 	// cleanup Vulkan resources
-    void cleanup();
+	void cleanup();
+
+	/*---------- VALIDATION LAYERS / DEBUG ----------*/
+
+	const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+	};
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
+
+	// Checks what validation layers are supported by device
+	bool checkValidationLayerSupport();
+
+	// Return a list of the required extensions.
+	std::vector<const char*> getRequiredExtensions();
+
+	// Setup the debug messenger for validation layers
+	void setupDebugMessenger();
+
+	// Create the debug messenger (extension function), Destroying the debug messenger is handled in cleanup() method using external method.
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+		const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+
+	// Helper method to populate the VkDebugUtilsMessengerCreateInfoEXT structure with the desired settings for the debug messenger
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+	// Helper method to load the function pointer for destroying the debug messenger, since it's an extension function and not automatically loaded by Vulkan
+	static void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+		auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+		if (func != nullptr) {
+			func(instance, debugMessenger, pAllocator);
+		}
+	}
+
+	// Callback function for debug messages from validation layers
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData) {
+
+		std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+		return VK_FALSE;
+	}
+
+	/*---------- RENDERING METHODS ----------*/
+
+	// main rendering loop
+    void mainLoop();	
 };
+
 #endif /* HelloTriangleApplication_h */
